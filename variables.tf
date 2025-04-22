11c6266d5b6a5e92f9e03371df01c3c678a019c3
sburripwc23 <sburri.consultant@newscorp.com>
2025-04-22 14:55:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,14 @@ variable "cluster_name" {
   default = "eks-unencrypted-demo"
 }
 
+variable "subnet_ids" {
+  default = ["subnet-071d24e7da7812932", "subnet-02848b6b3026b21a4"]
+}
+
+variable "vpc_id" {
+  default = "vpc-014ccc486a24d5676"
+}
+
 variable "node_instance_type" {
   default = "t3.medium"
 }
