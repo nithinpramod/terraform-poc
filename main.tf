4c49b66cf9490dd7e316e82657e0c6ffa72e6d3d
sburripwc23 <sburri.consultant@newscorp.com>
2025-04-22 14:56:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = var.region
+}
+
+# EKS Cluster and Node Group
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.11.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  subnet_ids = var.subnet_ids
+  vpc_id     = var.vpc_id
+
+  eks_managed_node_groups = {
+    unencrypted_nodes = {
+      desired_size = var.desired_capacity
+      max_size     = 3
+      min_size     = 1
+
+      instance_types = [var.node_instance_type]
+      disk_size      = 20  # EBS volume, unencrypted by default
+
+      tags = {
+        Name = "eks-node-unencrypted"
+      }
+    }
+  }
+
+  enable_irsa = true
+
+  tags = {
+    Environment = "nonprod"
+    Terraform   = "true"
+  }
+}
