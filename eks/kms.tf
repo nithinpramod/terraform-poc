be58669e2b3c26bb558647af91585edaedfdbdea
Nithin N P <nithin.n.p@pwc.com>
2025-04-30 17:54:32 +0530

    test

--- eks/kms.tf
+++ eks/kms.tf
@@ -1,31 +1,31 @@
 
 
-resource "aws_kms_key" "eks_cmk" {
-  description             = "Customer Managed Key for encryption(EKS)"
-  deletion_window_in_days = 30
+# resource "aws_kms_key" "eks_cmk" {
+#   description             = "Customer Managed Key for encryption(EKS)"
+#   deletion_window_in_days = 30
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Id": "key-default-1",
-  "Statement": [
-    {
-      "Sid": "Enable IAM User Permissions",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-      },
-      "Action": "kms:*",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Id": "key-default-1",
+#   "Statement": [
+#     {
+#       "Sid": "Enable IAM User Permissions",
+#       "Effect": "Allow",
+#       "Principal": {
+#         "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+#       },
+#       "Action": "kms:*",
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-resource "aws_kms_alias" "cmk_alias_eks" {
-  name          = "alias/eks-cmk-key"
-  target_key_id = aws_kms_key.eks_cmk.key_id
-}
+# resource "aws_kms_alias" "cmk_alias_eks" {
+#   name          = "alias/eks-cmk-key"
+#   target_key_id = aws_kms_key.eks_cmk.key_id
+# }
 
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
